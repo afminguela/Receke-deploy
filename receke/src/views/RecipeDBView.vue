6863d0d8e112e90e605adb2babbc8a5420e8ab3b
Jordi <jordijs6@gmail.com>
2024-05-29 11:39:51 +0200

    merge: restore changes from dev to feature/delete

--- receke/src/views/RecipeDBView.vue
+++ receke/src/views/RecipeDBView.vue
@@ -98,8 +98,13 @@ export default {
 
             <!-- Ingredients list -->
             <div class="ingredients-list" v-for="(item, index) in recipeSelected.ingredients" :key="index">
+<<<<<<< HEAD
                 <div>
                     <img :src="`/media/${item.image}`">
+=======
+                
+                    <img :src="item.image">
+>>>>>>> 0c5670f (merge: restore changes from dev to feature/delete)
                     <h4> {{ item.ingredient }} {{ item.quantity }}</h4>
                 
             </div>
@@ -130,9 +135,12 @@ export default {
 
             <div class="stars">
                 <h3>Valora esta receta</h3>
-                <div class="stars-container"
-                    :class="starsSelected === 1 ? 'selected1' : starsSelected === 2 ? 'selected2' : starsSelected === 3 ? 'selected3' : starsSelected === 4 ? 'selected4' : starsSelected === 5 ? 'selected5' : ''">
-                    <div class="star" v-for="star in 5" :key="star" @click="checkStars(star)">
+                <div class="stars-container" :class="starsSelected === 1? 'selected1' : starsSelected === 2? 'selected2' : starsSelected === 3? 'selected3' : starsSelected === 4? 'selected4' : starsSelected === 5? 'selected5': ''">
+                    <div class="star"  
+                        v-for="star in 5" 
+                        :key="star" 
+                        
+                        @click="checkStars(star)">
                         <!-- <p>{{ star }}</p> -->
                     </div>
                 </div>
